ac1b62af6bd005d82df01884ddae2bc01db916b0
pjfl <github@roxsoft.co.uk>
2015-09-02 11:47:38 +0100

    Toolchain update

--- lib/Data/CloudWeights.pm
+++ lib/Data/CloudWeights.pm
@@ -2,7 +2,7 @@ package Data::CloudWeights;
 
 use 5.01;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.15.%d', q$Rev: 1 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.15.%d', q$Rev: 2 $ =~ /\d+/gmx );
 
 use POSIX;
 use Type::Utils     qw( as enum message subtype where );
@@ -287,7 +287,7 @@ Data::CloudWeights - Calculate values for an HTML tag cloud
 
 =head1 Version
 
-Describes version v0.15.$Rev: 1 $ of L<Data::CloudWeights>
+Describes version v0.15.$Rev: 2 $ of L<Data::CloudWeights>
 
 =head1 Synopsis
 
